891ddf7fcdf3ba208d75ec93d3a4cf11983e8fdc
oger000 <gerhard.oettl@ogersoft.at>
2018-03-23 11:11:18 +0100

    license

--- ogerlib.js
+++ ogerlib.js
@@ -1,5 +1,9 @@
 /**
 #LICENSE BEGIN
+**********************************************************************
+* OgerArch - Archaeological Database is released under the GNU General Public License (GPL) <http://www.gnu.org/licenses>
+* Copyright (C) Gerhard Öttl <gerhard.oettl@ogersoft.at>
+**********************************************************************
 #LICENSE END
 */
 
